e0f1e8884a37ae4f914945986ba6122b7953963d
Killer-kate <75667457+Killer-kate@users.noreply.github.com>
2022-04-02 15:01:14 +0300

    create P

--- mapnote/src/app/app.component.ts
+++ mapnote/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'mapnote';
+  public attrbutes = { id: 'hello' };
+
+  public onClick() {
+    this.attrbutes = { id: 'paragraph' };
+  }
 }
